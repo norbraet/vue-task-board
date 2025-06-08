7871ee154c28b17af0391c328d1078dafa1b0ce7
Norbert Balaz <n.balaz@s-v.de>
2025-06-09 01:48:48 +0200

    style: update TaskCard component background color to use semantic variable

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -7,7 +7,7 @@ const emit = defineEmits(['remove'])
 </script>
 
 <template>
-    <div class="bg-white p-4 rounded mb-2 shadow">
+    <div class="bg-background p-4 rounded mb-2 shadow">
         <h4>{{ props.task.title }}</h4>
         <p>#{{ props.task.ticketNumber }}</p>
         <button @click="emit('remove', props.task)" class="text-red-500 text-xs mt-2 p-2 hover:outline-solid">Löschen</button>
