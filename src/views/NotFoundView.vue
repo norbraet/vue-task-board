152c4c599891b91ec930d31328b27f520faeaa70
Norbert Balaz <n.balaz@outlook.com>
2025-06-11 20:27:41 +0200

    feat: add 404 page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <section class="flex h-96 flex-col items-center justify-center text-center">
+        <h1 class="text-xl font-bold">404 Not Found</h1>
+        <p class="mb-5 text-xl">This page does not exist</p>
+        <RouterLink
+            to="/"
+            class="mt-4 rounded-md bg-blue-600 px-3 py-2 text-white hover:bg-blue-700"
+        >
+            Go Back
+        </RouterLink>
+    </section>
+</template>
