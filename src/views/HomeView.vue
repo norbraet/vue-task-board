05ea518864b4900cf634cb057adaa7b101e98ff1
Norbert Balaz <n.balaz@s-v.de>
2025-06-12 13:33:41 +0200

    chore: formats code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
-import TheWelcome from '@/components/HelloWorld.vue'
+    import HelloWorld from '@/components/HelloWorld.vue'
+    import TheWelcome from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <main>
-    <section class="container mx-auto flex flex-col gap-4">
-      <HelloWorld msg="Hello world!" />
-    </section>
-  </main>
+    <main>
+        <section class="container mx-auto flex flex-col gap-4">
+            <HelloWorld msg="Hello world!" />
+        </section>
+    </main>
 </template>
